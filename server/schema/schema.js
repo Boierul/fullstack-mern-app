44c75d64a9ebdc9837c4dec675e0d5f094b5aa56
Boierul <49337867+Boierul@users.noreply.github.com>
2022-11-11 19:07:22 +0100

    Implemented all the mutations for the Client.

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -2,7 +2,8 @@
 const Project = require('../models/Project')
 const Client = require('../models/Client')
 
-const {GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema, GraphQLList} = require('graphql')
+const {GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema, GraphQLList, GraphQLNonNull} = require('graphql')
+const {extendSchemaImpl} = require("graphql/utilities/extendSchema");
 
 // Client type -> set all the field of the requested GQL obj
 const ClientType = new GraphQLObjectType({
@@ -82,6 +83,81 @@ const RootQuery = new GraphQLObjectType({
     }
 })
 
+
+// Make mutations (POST,PATCH,DELETE)
+// The resolver will return the necessary data if needed
+const RootMutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        // Add Client - POST
+        addClient: {
+            type: ClientType,
+            args: {
+                name: {type: GraphQLNonNull(GraphQLString)},
+                email: {type: GraphQLNonNull(GraphQLString)},
+                phone: {type: GraphQLNonNull(GraphQLString)}
+            },
+            resolve(parent, args) {
+                // Create a new Client mongoose model
+                const client = new Client({
+                    name: args.name,
+                    email: args.email,
+                    phone: args.phone
+                })
+
+                return client.save()
+
+                // Alternatively this could be done by returning:
+                //     return Client.create({
+                //                     name: args.name,
+                //                     email: args.email,
+                //                     phone: args.phone
+                //                 })
+            }
+        },
+
+        // Delete a Client - DELETE
+        deleteClient: {
+            type: ClientType,
+            args: {
+                id: {type: GraphQLNonNull(GraphQLID)}
+            },
+            resolve(parent, args) {
+                // Delete the projects related to the client
+                Project.find({clientId: args.id}).then((projects) => {
+                    projects.forEach((project) => {
+                        project.remove();
+                    });
+                });
+
+                // Mongoose method of removing an object
+                return Client.findByIdAndRemove(args.id)
+            }
+        },
+
+        // Update a Client - PATCH
+        updateClient: {
+            type: ClientType,
+            args: {
+                id: {type: GraphQLNonNull(GraphQLID)},
+                name: {type:GraphQLNonNull(GraphQLString)},
+                email: {type:GraphQLNonNull(GraphQLString)},
+                phone: {type:GraphQLNonNull(GraphQLString)},
+            },
+            resolve(parent, args) {
+                return Client.findByIdAndUpdate(args.id, {
+                    // Update query
+                    name: args.name,
+                    email: args.email,
+                    phone: args.phone
+                })
+            }
+        }
+
+    }
+})
+
 module.exports = new GraphQLSchema({
-    query: RootQuery
+    query: RootQuery,
+    mutation: RootMutation
 })

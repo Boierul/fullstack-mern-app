5986ea9a97da0ebcdd71ade05e03a4a61446e571
Boierul <49337867+Boierul@users.noreply.github.com>
2022-11-11 13:41:29 +0100

    Added additional queries && queries-mutations-examples.txt.

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -2,7 +2,7 @@
 
 const {projects, clients} = require('../sampleData')
 
-const {GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema} = require('graphql')
+const {GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema, GraphQLList} = require('graphql')
 
 // Client type -> set all the field of the requested GQL obj
 const ClientType = new GraphQLObjectType({
@@ -15,11 +15,38 @@ const ClientType = new GraphQLObjectType({
     })
 })
 
+// Project type -> set all the field of the requested GQL obj
+const ProjectType = new GraphQLObjectType({
+    name: 'Project',
+    fields: () => ({
+        id: {type: GraphQLID},
+        name: {type: GraphQLString},
+        description: {type: GraphQLString},
+        status: {type: GraphQLString},
+        client: {
+            // The type is custom and will be dynamically fetched with the request
+            type: ClientType,
+            resolve(parent, args) {
+                return clients.find(client => client.id === parent.clientId)
+            }
+        }
+    })
+})
+
 // To make a query, root object is necessary, here we configure the query
 // The resolver will return the necessary data
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
+        // GET all client
+        clients: {
+            type: new GraphQLList(ClientType),
+            resolve(parent, args) {
+                return clients
+            }
+        },
+
+        // GET client by ID
         client: {
             type: ClientType,
             args: {
@@ -30,6 +57,28 @@ const RootQuery = new GraphQLObjectType({
             resolve(parent, args) {
                 // Insert GQL query from DB
                 return clients.find(client => client.id === args.id)
+            }
+        },
+
+        // GET all projects
+        projects: {
+            type: new GraphQLList(ProjectType),
+            resolve(parent, args) {
+                return projects
+            }
+        },
+
+        // GET project by ID
+        project: {
+            type: ProjectType,
+            args: {
+                id: {
+                    type: GraphQLID
+                }
+            },
+            resolve(parent, args) {
+                // Insert GQL query from DB
+                return projects.find(project => project.id === args.id)
             }
         }
     }

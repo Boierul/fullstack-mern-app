2d9d230ded4e3e45a4f0e8d32460b5e61b95ab8d
Boierul <49337867+Boierul@users.noreply.github.com>
2022-11-11 13:10:57 +0100

    Implemented a query to the sampleData.js (DB alternative, for now).

--- server/index.js
+++ server/index.js
@@ -1,15 +1,19 @@
 const express = require('express')
 const {graphqlHTTP} = require('express-graphql')
 
-const schema = require('schema/schema')
+const schema = require('./schema/schema')
 const port = process.env.PORT || 5000
 const app = express()
 
+// GraphiQL (like POSTMAN but for GQL)should be set to process.env.NODE_ENV === 'development' instead of true,
+// in order to set the env var of the project. But the thing is not working,
+// even with dotenv lib.
+
 app.use('/graphql', graphqlHTTP({
     schema,
-    graphiql: process.env.NODE_ENV === 'development',
+    graphiql: true,
 }))
 
 app.listen(port, () => {
-    console.log(`Server starting on port: ${port}`)
+    console.log(`Server started on port: ${port}`)
 })
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -2,12 +2,39 @@
 
 const {projects, clients} = require('../sampleData')
 
-const {GraphQLObjectType} = require('graphql')
+const {GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema} = require('graphql')
 
-// Client type
+// Client type -> set all the field of the requested GQL obj
 const ClientType = new GraphQLObjectType({
     name: 'Client',
-    fields: () => {
+    fields: () => ({
+        id: {type: GraphQLID},
+        name: {type: GraphQLString},
+        email: {type: GraphQLString},
+        phone: {type: GraphQLString}
+    })
+})
 
+// To make a query, root object is necessary, here we configure the query
+// The resolver will return the necessary data
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        client: {
+            type: ClientType,
+            args: {
+                id: {
+                    type: GraphQLID
+                }
+            },
+            resolve(parent, args) {
+                // Insert GQL query from DB
+                return clients.find(client => client.id === args.id)
+            }
+        }
     }
+})
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
 })
